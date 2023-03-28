57a35d1b16fdb3215ebb1bd8cad3552504e54ba1
Gropp <fgropp@gmail.com>
2023-03-28 11:06:46 -0300

    configurando banco e acessos

---
+++ src/database/config/config.js
@@ -0,0 +1,17 @@
+//migrando a configuracao para o js para que o .env possa enxergar
+require('dotenv').config({path: __dirname+'/./../../../.env'});
+
+module.exports = {
+    development: {
+        username: process.env.DATABASE_USER,
+        password: process.env.PASSWORD,
+        database: process.env.DATABASE,
+        host: '127.0.0.1',
+        dialect: 'mysql',
+        logging: false,
+        define: {
+            timestamps: true
+        },
+        timezone: '-03:00'
+    }
+}
